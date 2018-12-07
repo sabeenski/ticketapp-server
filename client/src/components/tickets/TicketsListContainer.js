ed2911d6da67c0b37c3dbcb187c6b1e907f8a798
sabeenski <sabinadhaugoda@gmail.com>
2018-12-07 07:10:00 +0100

    tickets of a event

--- client/src/components/tickets/TicketsListContainer.js
+++ client/src/components/tickets/TicketsListContainer.js
@@ -12,25 +12,24 @@ import LoginForm from '../login/LoginForm';
 
 class TicketsListContainer extends Component {
 
-  componentDidMount() {
+  componentWillMount() {
       this.props.loadTickets(Number(this.props.match.params.id))
-      this.props.loadEvents()
 
   }
-
+  
   
 
 
   render() { 
-
     if(!this.props.tickets) return `There are no tickets for this event at the moment.`
      else {
        return ( 
          <div className="container">
            {/* <h4>Tickets for : {this.props.events && this.props.events[this.props.match.params.id].name} </h4> */}
-           {this.props.tickets && this.props.tickets.map(ticket => {
+           {this.props.tickets.map(ticket => {
              return (
-                <div key={ticket.id}>
+                <div key={ticket.id}> 
+                  
                   <div> <h4>-----------------------------------</h4>
                     <b>Ticket number: </b> <Link to={`/tickets/${ticket.id}`}> {ticket.id} </Link>
                     <p>Price: {ticket.price} </p>
@@ -38,13 +37,13 @@ class TicketsListContainer extends Component {
                   </div>
                 </div>   
               )
-             
+
            })}
            
          </div>
 
-      );
-  }
+);
+}
 }
 }
  
